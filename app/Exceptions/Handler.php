67e9c11b655461604fa451769c45333ebc23ba22
Victor Emanuel Del Castillo <emanuel.delcastillo@gmail.com>
2018-09-04 14:33:30 -0400

    Fixes exertion error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-
+use Spatie\Permission\Exceptions\UnauthorizedException;
 class Handler extends ExceptionHandler
 {
     /**
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ( $exception instanceof UnauthorizedException ) {
+            return response()->json(['User has not permission for this page access.']);
+        }
         return parent::render($request, $exception);
     }
 }
